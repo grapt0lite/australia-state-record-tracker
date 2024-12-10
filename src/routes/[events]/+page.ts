59bf45cce11c0fcd11e95192c79991fd865eb77a
grapt0lite <iamnotsafe@proton.me>
2024-12-10 21:01:40 +0800

    Added vercel.json

--- src/routes/[events]/+page.ts
+++ src/routes/[events]/+page.ts
@@ -1,5 +1,5 @@
 export const load = ({ data, params }) => {
-    const chosen_event = params.events;
+    const chosen_event: string = params.events;
     return {
       chosen_event,
       ...data
