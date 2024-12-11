42f42fd11f9d70707fedcd0fb39864166bc96b67
grapt0lite <iamnotsafe@proton.me>
2024-12-11 15:16:28 +0800

    Added node target for Pages
    Cloudflare pages build stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +14,14 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	kit: {
+		adapter: adapter({
+		  esbuild: {
+			platform: 'node', // Ensure it targets Node.js
+		  },
+		}),
+	},
 };
 
 export default config;
