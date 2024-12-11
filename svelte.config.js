7eaf9de9b457bc368a4072c8e7cedb6a6c4d4da0
grapt0lite <iamnotsafe@proton.me>
2024-12-11 15:22:50 +0800

    asd
    ads

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,15 +12,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	kit: {
 		adapter: adapter({
-		  esbuild: {
-			platform: 'node', // Ensure it targets Node.js
-		  },
-		}),
-	},
+			esbuild: {
+			  platform: 'node', // Ensure it targets Node.js
+			},
+		  }),
+	}
 };
 
 export default config;
