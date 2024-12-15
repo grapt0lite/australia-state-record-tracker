69ef0cf1c272fcb1ce7c6259da6946fcd847ef47
grapt0lite <ethanannane@proton.me>
2024-12-15 12:20:20 +0800

    Fixed ranking for Mblind and others
    ya

--- src/routes/[events]/+page.server.ts
+++ src/routes/[events]/+page.server.ts
@@ -11,19 +11,66 @@ export async function load({ params }) {
     const chosen_event: string = params.events;
     // Explicitly cast the response to Track[] after the query execution
     const response_single = await sql`
-    SELECT * 
-    FROM public.records 
-    WHERE record_event LIKE ${chosen_event}
-        AND r_type LIKE 'single'
-    ORDER BY record_time ASC;
+SELECT *,
+       CASE 
+           WHEN record_time LIKE '%/% %' THEN 
+               -- Compute the ranking value for 12/13 45:56.32
+               CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 1), '/', 1) AS NUMERIC) - 
+               (CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 1), '/', 2) AS NUMERIC) - 
+                CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 1), '/', 1) AS NUMERIC))
+           ELSE 
+               -- Default ranking value for normal times
+               0
+       END AS rank_value,
+       CASE 
+           WHEN record_time LIKE '%/% %' THEN 
+               -- Convert minute:second.millisecond to total seconds for 12/13 format
+               (CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 2), ':', 1) AS NUMERIC) * 60) + 
+               CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 2), ':', 2) AS NUMERIC)
+           WHEN record_time LIKE '%:%' THEN 
+               -- Convert minute:second.millisecond to total seconds for regular times
+               (CAST(SPLIT_PART(record_time, ':', 1) AS NUMERIC) * 60) + 
+               CAST(SPLIT_PART(record_time, ':', 2) AS NUMERIC)
+           ELSE 
+               -- Assume it's already in seconds for regular times
+               CAST(record_time AS NUMERIC)
+       END AS time_in_seconds
+FROM public.records
+WHERE record_event LIKE ${chosen_event}
+  AND r_type LIKE 'single'
+ORDER BY rank_value ASC, time_in_seconds ASC;
+
     ` as unknown as Record[];
 
     const response_average = await sql`
-    SELECT * 
-    FROM public.records 
-    WHERE record_event LIKE ${chosen_event}
-        AND r_type LIKE 'average'
-    ORDER BY record_time ASC;
+SELECT *,
+       CASE 
+           WHEN record_time LIKE '%/% %' THEN 
+               -- Compute the ranking value for 12/13 45:56.32
+               CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 1), '/', 1) AS NUMERIC) - 
+               (CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 1), '/', 2) AS NUMERIC) - 
+                CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 1), '/', 1) AS NUMERIC))
+           ELSE 
+               -- Default ranking value for normal times
+               0
+       END AS rank_value,
+       CASE 
+           WHEN record_time LIKE '%/% %' THEN 
+               -- Convert minute:second.millisecond to total seconds for 12/13 format
+               (CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 2), ':', 1) AS NUMERIC) * 60) + 
+               CAST(SPLIT_PART(SPLIT_PART(record_time, ' ', 2), ':', 2) AS NUMERIC)
+           WHEN record_time LIKE '%:%' THEN 
+               -- Convert minute:second.millisecond to total seconds for regular times
+               (CAST(SPLIT_PART(record_time, ':', 1) AS NUMERIC) * 60) + 
+               CAST(SPLIT_PART(record_time, ':', 2) AS NUMERIC)
+           ELSE 
+               -- Assume it's already in seconds for regular times
+               CAST(record_time AS NUMERIC)
+       END AS time_in_seconds
+FROM public.records
+WHERE record_event LIKE ${chosen_event}
+  AND r_type LIKE 'average'
+ORDER BY rank_value ASC, time_in_seconds ASC;
     ` as unknown as Record[];
 
     return {
